4b4ffcd8f93407ad13cf324a12d522131461a66e
Gustavo Martinez <gmtz.0794@gmail.com>
2023-11-13 18:56:15 -0800

    fix first page and css for note page

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// HTML Routes
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// API Routes
+app.get('/api/notes', (req, res) => {
+  const notes = JSON.parse(fs.readFileSync('db.json', 'utf8'));
+  res.json(notes);
+});
+
+app.post('/api/notes', (req, res) => {
+  const newNote = req.body;
+  const notes = JSON.parse(fs.readFileSync('db.json', 'utf8'));
+  notes.push(newNote);
+  fs.writeFileSync('db.json', JSON.stringify(notes));
+  res.json(newNote);
+});
+
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT ${PORT}`);
+});
